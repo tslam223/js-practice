2217bd8ac08616a038821c75ffbdfbf62308a7c7
tslam223 <tslam223@gmail.com>
2024-06-21 19:48:50 +0100

    JS-PRACTICE

---
+++ Practice.js
@@ -0,0 +1,10 @@
+var taiwo = document.getElementById("taiwo");
+var color = document.getElementById("color");
+
+const changeText = function(){ // added function keyword
+  taiwo.innerHTML = "My name is kslam";
+}
+
+function changecolor(){ // already had function keyword
+  color.style.color = "blue"; // changed kenny to color
+}
